f68c5d4ccd19def8aca9e354695eccc1e1878bce
Vignesh Murugeshkumar <vigneshmurugeshkumar@gmail.com>
2026-04-07 09:12:03 +0530

    UC3

--- trainconstraintmanagement/src/TrainConsistManagementApp.java
+++ trainconstraintmanagement/src/TrainConsistManagementApp.java
@@ -1,15 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
-
-        System.out.println("========================================");
-        System.out.println("    === Train Consist Management App ===");
-        System.out.println("========================================\n");
-        List<String> trainConsist = new ArrayList<>();
-        System.out.println("Train initialized successfully...");
-        System.out.println("Initial Bogie Count : " + trainConsist.size());
-        System.out.println("Current Train Consist : " + trainConsist);
-        System.out.println("\nSystem ready for operations...");
+        System.out.println(" ======================================= ");
+        System.out.println(" UC3 - Track Unique Bogie IDs ");
+        System.out.println(" ======================================= \n");
+        Set<String> bogies = new HashSet<>();
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG103");
+        bogies.add("BG104");
+        bogies.add("BG101");
+        bogies.add("BG102");
+        bogies.add("BG105");
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogies);
+        System.out.println("\nNote:");
+        System.out.println("Duplicates are automatically ignored by HashSet.");
+        System.out.println("\nUC3 uniqueness validation completed ...");
     }
 }
